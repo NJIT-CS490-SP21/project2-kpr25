6c754cdedcfeaf55ca1af56ed0c1e0e7ebcf1890
EC2 Default User <ec2-user@ip-172-31-50-188.ec2.internal>
2021-02-26 20:45:13 +0000

    Add socket into file

--- app.py
+++ app.py
@@ -1,17 +1,49 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, json, session
+from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='./build/static')
 
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+socketio = SocketIO(
+    app,
+    cors_allowed_origins="*",
+    json=json,
+    manage_session=False
+)
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
 
+@socketio.on('connect')
+def on_connect():
+    print('User connected!')
 
-app.run(
+@socketio.on('disconnect')
+def on_disconnect():
+    print('User disconnected!')
+
+@socketio.on('game')
+def TikTac(data): 
+    print(data)
+    socketio.emit('game',  data, broadcast=True, include_self=False)
+
+@socketio.on('login')
+def on_chat(data):
+    print(str(data))
+    socketio.emit('login',  data, broadcast=True, include_self=False)
+
+@socketio.on('Restart')
+def Game(data):
+    print(str(data))
+    socketio.emit('Restart',  data, broadcast=True, include_self=False)
+
+socketio.run(
+    app,
     host=os.getenv('IP', '0.0.0.0'),
     port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
-    debug=True
 )
