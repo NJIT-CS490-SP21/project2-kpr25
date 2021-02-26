6fec8238743b366bc311baf8cd5e1c6afd456b6d
EC2 Default User <ec2-user@ip-172-31-50-188.ec2.internal>
2021-02-26 20:53:23 +0000

    Add a better layout of the code

--- src/TheSquares.js
+++ src/TheSquares.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { inside } from './App.css';
 
 const Square = ({ value, onClick }) => (
-    <button class="inside"
-        onClick={onClick}>
-        {value}
+    <button 
+        class="inside"
+        onClick={onClick}>{value}
     </button>
 );
 
