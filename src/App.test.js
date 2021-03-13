c6c89e65af467fc8d6ad6b6974b898cc12d1eb9a
EC2 Default User <ec2-user@ip-172-31-50-188.ec2.internal>
2021-03-13 22:01:56 +0000

    Add Jest Unit Tests Updated File

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('The login is gone', () => {
+  const result = render(<App />);
+
+  const LoginButton = screen.getByText('Login');
+  expect(LoginButton).toBeInTheDocument();
+
+  fireEvent.click(LoginButton);
+  expect(LoginButton).toBeInTheDocument();
+
+  expect(screen.getByText('Login'));
+});
+
+test('The Board Shows up', () => {
+  const result = render(<App />);
+
+  const PlayButton = screen.getByText('Play');
+  expect(PlayButton).toBeInTheDocument();
+
+  fireEvent.click(PlayButton);
+  expect(PlayButton).toBeInTheDocument();
+
+  expect(screen.getByText('Play'));
+});
+
+test('The Leaderboard shows Up', () => {
+  const result = render(<App />);
+
+  const LeaderboardButton = screen.getByText('Join the Leaderboard');
+  expect(LeaderboardButton).toBeInTheDocument();
+
+  fireEvent.click(LeaderboardButton);
+  expect(LeaderboardButton).toBeInTheDocument();
+
+  expect(screen.getByText('Join the Leaderboard'));
 });
